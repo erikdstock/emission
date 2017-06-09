216040fba31f21ec598dc326cee60db9a7981c7e
Eloy Durán <eloy.de.enige@gmail.com>
2017-06-09 13:38:04 +0200

    [Relay] Rename all imports to use Classic mode.

--- src/lib/Containers/Inbox.tsx
+++ src/lib/Containers/Inbox.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import * as Relay from "react-relay"
+import * as Relay from "react-relay/classic"
 
 import { ScrollView } from "react-native"
 import ActiveBids from "../Components/Inbox/Bids"
